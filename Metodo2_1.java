f0c2ac8dfa8fd1305b4f5ca3b17e1cd2794facd3
Alex Mora <162t0308@itsm.edu.mx>
2017-02-11 17:34:03 -0600

    Create Metodo2_1.java

---
+++ Metodo2_1.java
@@ -0,0 +1,11 @@
+public class Metodo2_1 {
+	public static void main(String []args){
+		sumar(3,4);
+	}
+		public static void sumar(int x,int y) {
+			
+			int suma = x+y;
+			System.out.println("El resultado de la suma es= "+suma);
+		
+}
+}
