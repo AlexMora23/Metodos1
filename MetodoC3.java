d70bcbccffb0da7d5bf81d3e2c8a16854c51c99c
Alex Mora <162t0308@itsm.edu.mx>
2017-02-11 22:53:36 -0600

    Create MetodoC3.java

---
+++ MetodoC3.java
@@ -0,0 +1,19 @@
+public class MetodoC3{
+	public static void main(String args[]){
+		int x = x();
+		int y = y();
+		String resul = "El resultado es "+resul(x,y);
+		System.out.println(resul);
+	}
+	public static int resul(int a, int b){
+		int resul= a+b;
+		return resul;
+		
+	}
+	public static int x(){
+		return 10;
+	}
+	public static int y(){
+		return 5;
+	}
+}
