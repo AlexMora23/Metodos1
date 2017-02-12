3785b0e6b1ce1484e18f331abd3e5b27c7b042f4
Alex Mora <162t0308@itsm.edu.mx>
2017-02-11 21:17:30 -0600

    Create Metodo3_1.java

---
+++ Metodo3_1.java
@@ -0,0 +1,14 @@
+public class Metodo3_1 {
+	public static void main(String []args){
+		int suma = suma();
+		System.out.println("El resultado de la suma es= "+suma);
+	}
+		public static int suma( ) {
+			int x=98;
+			int y=67;
+			int suma = x+y;
+			return suma;
+			
+		
+}
+}
