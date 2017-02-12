6850071ff69a29396b0fb9a53af43580b093680f
Alex Mora <162t0308@itsm.edu.mx>
2017-02-11 22:54:51 -0600

    Create MetodoC3_3.java

---
+++ MetodoC3_3.java
@@ -0,0 +1,20 @@
+
+public class MetodoC3_3{
+	public static void main(String args[]){
+	    double x = x();
+		double y = y();
+		String div = "El resultado es "+div(x,y);
+		System.out.println(div);
+	}
+	public static double div(double a, double b){
+		double div= a/b;
+		return div;
+		
+	}
+	public static int x(){
+		return 843;
+	}
+	public static int y(){
+		return 69;
+	}
+}
